55fa6237e053702115c804baea93c813eac32c80
Deborah <debby71711@gmail.com>
2023-01-12 17:02:00 +0100

    initial commit

---
+++ assignments/Square.py
@@ -0,0 +1,8 @@
+def asterisk(number):
+    for i in range(number):
+      j = number * "*"
+
+      print(j)
+
+
+asterisk(4)
---
+++ assignments/Triangle.py
@@ -0,0 +1,7 @@
+def asterisk(number):
+    for i in range(1, number, 2):
+        j = i * "*"
+        print(j)
+
+
+asterisk(21)
